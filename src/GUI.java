9319b840a6251d77f934301a0206aeb71050ee4c
yoan chaillan <yoan chaillan>
2017-04-20 15:08:25 +0200

    changement de type de la GUI ->CardLayout : facilite le changement de Panel ("changement de page")

--- src/GUI.java
+++ src/GUI.java
@@ -22,9 +22,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.UIManager;
+import java.awt.CardLayout;
+
+import javax.swing.JPanel;
 
 public class GUI {
-
+	
+	private CardLayout cardLayout;
 	private JFrame frmTer;
 	private JTextField textFieldOutput;
 	private JTextField textFieldTemplate;
@@ -66,34 +70,214 @@ public class GUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		cardLayout = new CardLayout(0, 0);
 		frmTer = new JFrame();
 		frmTer.setTitle("TER 2017");
 		frmTer.setBounds(100, 100, 550, 720);
 		frmTer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		SpringLayout springLayout = new SpringLayout();
-		frmTer.getContentPane().setLayout(springLayout);
+		frmTer.getContentPane().setLayout(cardLayout);
+		
+		
+		/**
+		 * PANEL 1 : 
+		 * -select files and output folder
+		 * -generate shuffled table
+		 * 
+		 * >next panel will show the table
+		 */
+		JPanel panel1 = new JPanel();
+		frmTer.getContentPane().add(panel1, "PANEL1");
+		SpringLayout sl_panel1 = new SpringLayout();
+		panel1.setLayout(sl_panel1);
 		
 		
 		JLabel lblFichierExcelxsl = new JLabel("Fichier excel (.xls) :");
-		frmTer.getContentPane().add(lblFichierExcelxsl);
-		
-		
-		JLabel lblTemplatesDeCv = new JLabel("Templates de CV (.doc) :");
-		springLayout.putConstraint(SpringLayout.SOUTH, lblFichierExcelxsl, -36, SpringLayout.NORTH, lblTemplatesDeCv);
-		springLayout.putConstraint(SpringLayout.EAST, lblFichierExcelxsl, 0, SpringLayout.EAST, lblTemplatesDeCv);
-		springLayout.putConstraint(SpringLayout.NORTH, lblTemplatesDeCv, 130, SpringLayout.NORTH, frmTer.getContentPane());
-		frmTer.getContentPane().add(lblTemplatesDeCv);
+		sl_panel1.putConstraint(SpringLayout.NORTH, lblFichierExcelxsl, 35, SpringLayout.NORTH, panel1);
+		sl_panel1.putConstraint(SpringLayout.SOUTH, lblFichierExcelxsl, 51, SpringLayout.NORTH, panel1);
+		panel1.add(lblFichierExcelxsl);
 		
 		
 		JLabel lblDossierDeDestination = new JLabel("Dossier de destination :");
-		springLayout.putConstraint(SpringLayout.EAST, lblTemplatesDeCv, 0, SpringLayout.EAST, lblDossierDeDestination);
-		springLayout.putConstraint(SpringLayout.WEST, lblDossierDeDestination, 20, SpringLayout.WEST, frmTer.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, lblDossierDeDestination, -167, SpringLayout.SOUTH, frmTer.getContentPane());
-		frmTer.getContentPane().add(lblDossierDeDestination);
+		panel1.add(lblDossierDeDestination);
+		
+		
+		JLabel lblTemplatesDeCv = new JLabel("Templates de CV (.doc) :");
+		sl_panel1.putConstraint(SpringLayout.NORTH, lblTemplatesDeCv, 34, SpringLayout.SOUTH, lblFichierExcelxsl);
+		panel1.add(lblTemplatesDeCv);
+		JButton btnGnrerLesCvs = new JButton("Générer les CVs");
+		sl_panel1.putConstraint(SpringLayout.SOUTH, lblDossierDeDestination, -44, SpringLayout.NORTH, btnGnrerLesCvs);
+		sl_panel1.putConstraint(SpringLayout.WEST, btnGnrerLesCvs, 70, SpringLayout.WEST, panel1);
+		sl_panel1.putConstraint(SpringLayout.SOUTH, btnGnrerLesCvs, -33, SpringLayout.SOUTH, panel1);
+		panel1.add(btnGnrerLesCvs);
+		
+		
+		textFieldOutput = new JTextField();
+		sl_panel1.putConstraint(SpringLayout.WEST, textFieldOutput, 171, SpringLayout.WEST, panel1);
+		sl_panel1.putConstraint(SpringLayout.SOUTH, textFieldOutput, -37, SpringLayout.NORTH, btnGnrerLesCvs);
+		sl_panel1.putConstraint(SpringLayout.EAST, lblDossierDeDestination, -12, SpringLayout.WEST, textFieldOutput);
+		panel1.add(textFieldOutput);
+		textFieldOutput.setEditable(false);
+		textFieldOutput.setColumns(10);
+		
+		
+		textFieldTemplate = new JTextField();
+		sl_panel1.putConstraint(SpringLayout.WEST, textFieldTemplate, 170, SpringLayout.WEST, panel1);
+		sl_panel1.putConstraint(SpringLayout.EAST, lblTemplatesDeCv, -9, SpringLayout.WEST, textFieldTemplate);
+		panel1.add(textFieldTemplate);
+		textFieldTemplate.setEditable(false);
+		textFieldTemplate.setColumns(10);
+		
+		
+		textFieldExcel = new JTextField();
+		sl_panel1.putConstraint(SpringLayout.WEST, textFieldExcel, 170, SpringLayout.WEST, panel1);
+		sl_panel1.putConstraint(SpringLayout.NORTH, textFieldTemplate, 22, SpringLayout.SOUTH, textFieldExcel);
+		sl_panel1.putConstraint(SpringLayout.EAST, lblFichierExcelxsl, -17, SpringLayout.WEST, textFieldExcel);
+		sl_panel1.putConstraint(SpringLayout.NORTH, textFieldExcel, 30, SpringLayout.NORTH, panel1);
+		sl_panel1.putConstraint(SpringLayout.SOUTH, textFieldExcel, 58, SpringLayout.NORTH, panel1);
+		panel1.add(textFieldExcel);
+		textFieldExcel.setEditable(false);
+		textFieldExcel.setColumns(10);
+		
+		
+		JButton btnParcourirTemplate = new JButton("Parcourir");
+		sl_panel1.putConstraint(SpringLayout.EAST, textFieldTemplate, -23, SpringLayout.WEST, btnParcourirTemplate);
+		panel1.add(btnParcourirTemplate);
+		
+		
+		JButton btnParcourirExcel = new JButton("Parcourir");
+		sl_panel1.putConstraint(SpringLayout.NORTH, btnParcourirTemplate, 22, SpringLayout.SOUTH, btnParcourirExcel);
+		sl_panel1.putConstraint(SpringLayout.WEST, btnParcourirTemplate, -95, SpringLayout.EAST, btnParcourirExcel);
+		sl_panel1.putConstraint(SpringLayout.EAST, btnParcourirTemplate, 0, SpringLayout.EAST, btnParcourirExcel);
+		sl_panel1.putConstraint(SpringLayout.EAST, textFieldExcel, -23, SpringLayout.WEST, btnParcourirExcel);
+		sl_panel1.putConstraint(SpringLayout.EAST, btnParcourirExcel, -124, SpringLayout.EAST, panel1);
+		sl_panel1.putConstraint(SpringLayout.NORTH, btnParcourirExcel, 30, SpringLayout.NORTH, panel1);
+		sl_panel1.putConstraint(SpringLayout.SOUTH, btnParcourirExcel, 58, SpringLayout.NORTH, panel1);
+		sl_panel1.putConstraint(SpringLayout.WEST, btnParcourirExcel, -219, SpringLayout.EAST, panel1);
+		panel1.add(btnParcourirExcel);
+		
+		
+		JButton btnParcourirOutput = new JButton("Parcourir");
+		sl_panel1.putConstraint(SpringLayout.EAST, textFieldOutput, -17, SpringLayout.WEST, btnParcourirOutput);
+		sl_panel1.putConstraint(SpringLayout.EAST, btnParcourirOutput, -129, SpringLayout.EAST, panel1);
+		sl_panel1.putConstraint(SpringLayout.NORTH, btnParcourirOutput, -126, SpringLayout.SOUTH, panel1);
+		sl_panel1.putConstraint(SpringLayout.SOUTH, btnParcourirOutput, -98, SpringLayout.SOUTH, panel1);
+		sl_panel1.putConstraint(SpringLayout.WEST, btnParcourirOutput, -224, SpringLayout.EAST, panel1);
+		panel1.add(btnParcourirOutput);
+		
+		
+		JScrollPane scrollPanelListeTemplate = new JScrollPane();
+		sl_panel1.putConstraint(SpringLayout.WEST, scrollPanelListeTemplate, 45, SpringLayout.WEST, panel1);
+		sl_panel1.putConstraint(SpringLayout.SOUTH, scrollPanelListeTemplate, -39, SpringLayout.NORTH, textFieldOutput);
+		sl_panel1.putConstraint(SpringLayout.EAST, scrollPanelListeTemplate, -39, SpringLayout.EAST, panel1);
+		panel1.add(scrollPanelListeTemplate);
+		
+		/*Tableau des templates*/
+		model = new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"Fichier", "Chemin", ""
+				}
+			);
+		/*Action de bouton supprimer du tableau*/
+		@SuppressWarnings("serial")
+		Action delete = new AbstractAction()
+		{
+		    public void actionPerformed(ActionEvent e)
+		    {
+		        JTable table = (JTable)e.getSource();
+		        int modelRow = Integer.valueOf( e.getActionCommand() );
+		        ((DefaultTableModel)table.getModel()).removeRow(modelRow);
+		        templates.remove(modelRow);
+		    }
+		};
+		
+		table = new JTable(model);
+		table.setBounds(377, 118, 480, 300);
+		table.setRowHeight(30);
+		@SuppressWarnings("unused")
+		ButtonColumn buttonColumn = new ButtonColumn(table, delete, 2);
+		scrollPanelListeTemplate.setViewportView(table);
+		
+		
+		JLabel lblListeDesTemplates = new JLabel("Liste des templates");
+		sl_panel1.putConstraint(SpringLayout.NORTH, scrollPanelListeTemplate, 4, SpringLayout.SOUTH, lblListeDesTemplates);
+		sl_panel1.putConstraint(SpringLayout.NORTH, lblListeDesTemplates, 45, SpringLayout.SOUTH, lblTemplatesDeCv);
+		sl_panel1.putConstraint(SpringLayout.SOUTH, lblListeDesTemplates, 61, SpringLayout.SOUTH, lblTemplatesDeCv);
+		sl_panel1.putConstraint(SpringLayout.WEST, lblListeDesTemplates, 49, SpringLayout.WEST, panel1);
+		panel1.add(lblListeDesTemplates);
+		
+		
+		JButton btnAjouter = new JButton("Ajouter");
+		sl_panel1.putConstraint(SpringLayout.NORTH, btnAjouter, 0, SpringLayout.NORTH, textFieldTemplate);
+		sl_panel1.putConstraint(SpringLayout.WEST, btnAjouter, 6, SpringLayout.EAST, btnParcourirTemplate);
+		sl_panel1.putConstraint(SpringLayout.EAST, btnAjouter, -23, SpringLayout.EAST, panel1);
+		panel1.add(btnAjouter);
+		
+		JButton btnNext = new JButton("NEXT");
+		sl_panel1.putConstraint(SpringLayout.WEST, btnNext, -219, SpringLayout.EAST, panel1);
+		sl_panel1.putConstraint(SpringLayout.EAST, btnNext, -160, SpringLayout.EAST, panel1);
+		btnNext.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				cardLayout.show(frmTer.getContentPane(),"PANEL2");
+			}
+		});
+		sl_panel1.putConstraint(SpringLayout.SOUTH, btnNext, 0, SpringLayout.SOUTH, btnGnrerLesCvs);
+		panel1.add(btnNext);
+		
+		
+		btnAjouter.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				addListeCV(templateName,templatePath);
+			}
+		});
+		
+		
+		btnParcourirOutput.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String folder = new String();
+				Explorer explorer = new Explorer("DIRECTORY");
+				folder = explorer.getFolder();
+				outputFolder = folder;
+						
+				textFieldOutput.setText(outputFolder);
+			}
+		});
+		
+		
+		/*Bouton parcourir pour le Excel .xls*/
+		btnParcourirExcel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Explorer explorer = new Explorer();
+				excelPath = explorer.getFilepath();
+				
+				textFieldExcel.setText(excelPath);
+			}
+		});
+		
+		
+		/*Bouton parcourir pour les templates de CV*/
+		btnParcourirTemplate.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Explorer explorer = new Explorer();
+				templateName = explorer.getFilename();
+				templatePath = explorer.getFilepath();
+				
+				
+				textFieldTemplate.setText(templatePath);
+				
+				//System.out.println("nom fichier récupéré = "+templateName);
+				//System.out.println("chemin fichier récupéré = "+templatePath);
+			}
+		});
 		
 		
 		/*Bouton pour générer les CVs*/
-		JButton btnGnrerLesCvs = new JButton("Générer les CVs");
 		btnGnrerLesCvs.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -121,159 +305,25 @@ public class GUI {
 				}
 			}
 		});
-		springLayout.putConstraint(SpringLayout.NORTH, btnGnrerLesCvs, -94, SpringLayout.SOUTH, frmTer.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, btnGnrerLesCvs, -50, SpringLayout.SOUTH, frmTer.getContentPane());
-		frmTer.getContentPane().add(btnGnrerLesCvs);
 		
 		
 		
-		textFieldOutput = new JTextField();
-		springLayout.putConstraint(SpringLayout.WEST, btnGnrerLesCvs, 0, SpringLayout.WEST, textFieldOutput);
-		springLayout.putConstraint(SpringLayout.EAST, btnGnrerLesCvs, 160, SpringLayout.WEST, textFieldOutput);
-		textFieldOutput.setEditable(false);
-		springLayout.putConstraint(SpringLayout.NORTH, textFieldOutput, -3, SpringLayout.NORTH, lblDossierDeDestination);
-		springLayout.putConstraint(SpringLayout.WEST, textFieldOutput, 6, SpringLayout.EAST, lblDossierDeDestination);
-		frmTer.getContentPane().add(textFieldOutput);
-		textFieldOutput.setColumns(10);
+
+
 		
 		
-		textFieldTemplate = new JTextField();
-		springLayout.putConstraint(SpringLayout.EAST, textFieldOutput, 0, SpringLayout.EAST, textFieldTemplate);
-		textFieldTemplate.setEditable(false);
-		springLayout.putConstraint(SpringLayout.WEST, textFieldTemplate, 6, SpringLayout.EAST, lblTemplatesDeCv);
-		springLayout.putConstraint(SpringLayout.EAST, textFieldTemplate, -230, SpringLayout.EAST, frmTer.getContentPane());
-		springLayout.putConstraint(SpringLayout.NORTH, textFieldTemplate, -3, SpringLayout.NORTH, lblTemplatesDeCv);
-		frmTer.getContentPane().add(textFieldTemplate);
-		textFieldTemplate.setColumns(10);
+		/**
+		 * PANEL 2 : 
+		 * -show the table the shuffled table
+		 */
+		
+		JPanel panel2 = new JPanel();
+		frmTer.getContentPane().add(panel2, "PANEL2");
+		panel2.setLayout(new SpringLayout());
 		
 		
-		textFieldExcel = new JTextField();
-		springLayout.putConstraint(SpringLayout.WEST, textFieldExcel, 6, SpringLayout.EAST, lblFichierExcelxsl);
-		springLayout.putConstraint(SpringLayout.EAST, textFieldExcel, 0, SpringLayout.EAST, textFieldTemplate);
-		textFieldExcel.setEditable(false);
-		springLayout.putConstraint(SpringLayout.NORTH, textFieldExcel, -3, SpringLayout.NORTH, lblFichierExcelxsl);
-		frmTer.getContentPane().add(textFieldExcel);
-		textFieldExcel.setColumns(10);
+
 		
-		
-		/*Bouton parcourir pour les templates de CV*/
-		JButton btnParcourirTemplate = new JButton("Parcourir");
-		springLayout.putConstraint(SpringLayout.NORTH, btnParcourirTemplate, -6, SpringLayout.NORTH, lblTemplatesDeCv);
-		springLayout.putConstraint(SpringLayout.WEST, btnParcourirTemplate, 16, SpringLayout.EAST, textFieldTemplate);
-		btnParcourirTemplate.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Explorer explorer = new Explorer();
-				templateName = explorer.getFilename();
-				templatePath = explorer.getFilepath();
-				
-				
-				textFieldTemplate.setText(templatePath);
-				
-				//System.out.println("nom fichier récupéré = "+templateName);
-				//System.out.println("chemin fichier récupéré = "+templatePath);
-			}
-		});
-		frmTer.getContentPane().add(btnParcourirTemplate);
-		
-		
-		/*Bouton parcourir pour le Excel .xls*/
-		JButton btnParcourirExcel = new JButton("Parcourir");
-		btnParcourirExcel.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Explorer explorer = new Explorer();
-				excelPath = explorer.getFilepath();
-				
-				textFieldExcel.setText(excelPath);
-			}
-		});
-		springLayout.putConstraint(SpringLayout.EAST, btnParcourirTemplate, 0, SpringLayout.EAST, btnParcourirExcel);
-		springLayout.putConstraint(SpringLayout.WEST, btnParcourirExcel, 16, SpringLayout.EAST, textFieldExcel);
-		springLayout.putConstraint(SpringLayout.EAST, btnParcourirExcel, -114, SpringLayout.EAST, frmTer.getContentPane());
-		springLayout.putConstraint(SpringLayout.NORTH, btnParcourirExcel, -4, SpringLayout.NORTH, lblFichierExcelxsl);
-		frmTer.getContentPane().add(btnParcourirExcel);
-		
-		
-		JButton btnParcourirOutput = new JButton("Parcourir");
-		springLayout.putConstraint(SpringLayout.EAST, btnParcourirOutput, 100, SpringLayout.WEST, btnParcourirTemplate);
-		btnParcourirOutput.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				String folder = new String();
-				Explorer explorer = new Explorer("DIRECTORY");
-				folder = explorer.getFolder();
-				outputFolder = folder;
-						
-				textFieldOutput.setText(outputFolder);
-			}
-		});
-		springLayout.putConstraint(SpringLayout.WEST, btnParcourirOutput, 0, SpringLayout.WEST, btnParcourirTemplate);
-		frmTer.getContentPane().add(btnParcourirOutput);
-		
-		
-		JScrollPane scrollPanelListeTemplate = new JScrollPane();
-		springLayout.putConstraint(SpringLayout.SOUTH, scrollPanelListeTemplate, -36, SpringLayout.NORTH, textFieldOutput);
-		springLayout.putConstraint(SpringLayout.NORTH, btnParcourirOutput, 36, SpringLayout.SOUTH, scrollPanelListeTemplate);
-		springLayout.putConstraint(SpringLayout.WEST, scrollPanelListeTemplate, 31, SpringLayout.WEST, frmTer.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, scrollPanelListeTemplate, -33, SpringLayout.EAST, frmTer.getContentPane());
-		frmTer.getContentPane().add(scrollPanelListeTemplate);
-		
-		
-		JLabel lblListeDesTemplates = new JLabel("Liste des templates");
-		springLayout.putConstraint(SpringLayout.NORTH, scrollPanelListeTemplate, 6, SpringLayout.SOUTH, lblListeDesTemplates);
-		springLayout.putConstraint(SpringLayout.NORTH, lblListeDesTemplates, 22, SpringLayout.SOUTH, textFieldTemplate);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblListeDesTemplates, 38, SpringLayout.SOUTH, textFieldTemplate);
-		
-		
-		/*Tableau des templates*/
-		model = new DefaultTableModel(
-				new Object[][] {
-				},
-				new String[] {
-					"Fichier", "Chemin", ""
-				}
-			);
-		table = new JTable(model);
-		
-		/*width and height*/
-		table.setBounds(377, 118, 480, 300);
-		int tableWidth = table.getWidth();
-		table.getColumnModel().getColumn(0).setMinWidth(Math.round(tableWidth*25/100));
-		table.getColumnModel().getColumn(1).setMinWidth(Math.round(tableWidth*50/100));
-		table.getColumnModel().getColumn(2).setMinWidth(Math.round(tableWidth*25/100));
-		table.setRowHeight(25);
-		
-		/*Action de bouton supprimer du tableau*/
-		@SuppressWarnings("serial")
-		Action delete = new AbstractAction()
-		{
-		    public void actionPerformed(ActionEvent e)
-		    {
-		        JTable table = (JTable)e.getSource();
-		        int modelRow = Integer.valueOf( e.getActionCommand() );
-		        ((DefaultTableModel)table.getModel()).removeRow(modelRow);
-		        templates.remove(modelRow);
-		    }
-		};
-		@SuppressWarnings("unused")
-		ButtonColumn buttonColumn = new ButtonColumn(table, delete, 2);
-		
-		scrollPanelListeTemplate.setViewportView(table);
-		springLayout.putConstraint(SpringLayout.WEST, lblListeDesTemplates, 106, SpringLayout.WEST, frmTer.getContentPane());
-		frmTer.getContentPane().add(lblListeDesTemplates);
-		
-		
-		JButton btnAjouter = new JButton("Ajouter");
-		btnAjouter.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				addListeCV(templateName,templatePath);
-			}
-		});
-		springLayout.putConstraint(SpringLayout.NORTH, btnAjouter, -6, SpringLayout.NORTH, lblTemplatesDeCv);
-		springLayout.putConstraint(SpringLayout.WEST, btnAjouter, 6, SpringLayout.EAST, btnParcourirTemplate);
-		frmTer.getContentPane().add(btnAjouter);
 	}
 	
 	
@@ -307,9 +357,4 @@ public class GUI {
 		}
 		//System.out.println("");
 	}
-	
-	
-	
-	
-	
 }
